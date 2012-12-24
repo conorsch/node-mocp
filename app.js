a2721d29b49e79dce69112fb791dc32549c10fa0
Conor Schaefer <conor.schaefer@gmail.com>
2012-12-23 23:58:19 -0500

    add express route handling

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 
-var express = require('express');
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , controls = require('./routes/controls')
+  , path = require('path');
+
 var app = express();
 
-//app.engine('.html', require('jade'));
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/users', user.list);
 
 var mocp = function( option ) {
 
@@ -34,5 +60,6 @@ var mocp = function( option ) {
   app.get('/play', mocp.play );
   app.get('/*', mocp.toggle );
 
-app.listen(3000);
-console.log('Listening on port 3000');
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
